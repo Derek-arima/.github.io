2bbed268da44324812da0c10a9c1d5963eaca3cf
Derek-arima <darima@byui.edu>
2021-01-21 21:26:30 -0700

    commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var date = document.lastModified
+document.getElementById("updated").innerHTML = date
+var d = new Date()
+var year = d.getFullYear()
+document.getElementById("year").innerHTML = year
